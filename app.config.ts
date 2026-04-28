e6f85cf395ba0f5886924b7aef53460a4b526dbf
syuto715 <syuto715@users.noreply.github.com>
2026-04-28 18:35:45 +0900

    chore(release): bump version to 1.0.2 for hotfix retry

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ import type { ExpoConfig } from '@expo/config-types';
 const config: ExpoConfig = {
   name: 'ミーリフト',
   slug: 'mealift',
-  version: '1.0.1',
+  version: '1.0.2',
   orientation: 'portrait',
   scheme: 'mealift',
   icon: './assets/icon.png',
